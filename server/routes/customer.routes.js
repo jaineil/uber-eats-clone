7088a8613514b2e7c57b047afe40d6c3d5155dde
jaineil <mandaviajaineil@gmail.com>
2021-10-26 02:40:36 -0700

    chore: small fixes

--- server/routes/customer.routes.js
+++ server/routes/customer.routes.js
@@ -5,7 +5,7 @@ const customerRoutes = express.Router();
 const customerController = new CustomerController();
 
 customerRoutes.post('/create-customer', customerController.createCustomer);
-customerRoutes.post('/update', customerController.updateCustomerMeta);
+customerRoutes.post('/update-customer', customerController.updateCustomerMeta);
 customerRoutes.get('/validate-customer', customerController.validateCustomerSignin);
 customerRoutes.get('/fetch-customer/:customerId', customerController.fetchCustomerMeta);
 customerRoutes.post('/add-favorite', customerController.addCustomerFavoriteRestaurant);
